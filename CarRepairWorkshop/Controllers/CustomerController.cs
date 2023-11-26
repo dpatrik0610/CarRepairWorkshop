72aec032ccd847145f3da6033fe3fb4df6075a26
dpatrik0610 <d.patrik0610@gmail.com>
2023-11-26 17:28:14 +0100

    Created CustomerController.cs to handle customer related requests

---
+++ CarRepairWorkshop/Controllers/CustomerController.cs
@@ -0,0 +1,104 @@
+﻿using CarRepairWorkshop.API.Services.Interfaces;
+using CarRepairWorkshop.Shared;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarRepairWorkshop.API.Controllers
+{ 
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly ICustomerService _customerService;
+
+        public CustomerController(ICustomerService customerService)
+        {
+            _customerService = customerService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Customer>>> GetAllCustomers()
+        {
+            try
+            {
+                var customers = await _customerService.GetAllCustomersAsync();
+                return Ok(customers);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Customer>> GetCustomerById(Guid id)
+        {
+            try
+            {
+                var customer = await _customerService.GetCustomerByIdAsync(id);
+
+                if (customer == null)
+                    return NotFound();
+
+                return Ok(customer);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> AddCustomer([FromBody] Customer customer)
+        {
+            try
+            {
+                await _customerService.AddCustomerAsync(customer);
+                return CreatedAtAction(nameof(GetCustomerById), new { id = customer.Id }, customer);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> UpdateCustomer(Guid id, [FromBody] Customer newCustomerData)
+        {
+            try
+            {
+                var existingCustomer = await _customerService.GetCustomerByIdAsync(id);
+
+                if (existingCustomer == null)
+                    return NotFound();
+
+                newCustomerData.Id = id; // Just to make sure it was not changed.
+                await _customerService.UpdateCustomerDataAsync(newCustomerData);
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteCustomer(Guid id)
+        {
+            try
+            {
+                var existingCustomer = await _customerService.GetCustomerByIdAsync(id);
+
+                if (existingCustomer == null)
+                    return NotFound();
+
+                await _customerService.DeleteCustomerAsync(id);
+
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex.Message}");
+            }
+        }
+    }
+}
