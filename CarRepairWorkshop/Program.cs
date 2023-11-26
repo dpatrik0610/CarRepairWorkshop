cf5da93d680f99e8dc6805318bd2f37a8f185f29
dpatrik0610 <d.patrik0610@gmail.com>
2023-11-26 16:06:21 +0100

    Registered WorkshopDbContext in Main file, using SQLite Database with Lazy Loading.

--- CarRepairWorkshop/Program.cs
+++ CarRepairWorkshop/Program.cs
@@ -1,15 +1,24 @@
+using Microsoft.EntityFrameworkCore;
 using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog(
+    (_, configuration) => configuration
+        .MinimumLevel.Information()
+    .WriteTo.Console()
+);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Host.UseSerilog(
-    (_, configuration) => configuration
-        .MinimumLevel.Information()
-        .WriteTo.Console()
-        );
+
+builder.Services.AddDbContext<WorkshopDbContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("SQLite"));
+    options.UseLazyLoadingProxies();
+});
 
 var app = builder.Build();
 
