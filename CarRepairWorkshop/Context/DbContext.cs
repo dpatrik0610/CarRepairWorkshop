c63d6264fb1fd202efb4564bcd890fdf1af8c499
dpatrik0610 <d.patrik0610@gmail.com>
2023-11-26 16:01:33 +0100

    Added dbcontext for workshop

--- CarRepairWorkshop/Context/DbContext.cs
+++ CarRepairWorkshop/Context/DbContext.cs
@@ -1,6 +1,19 @@
-﻿namespace CarRepairWorkshop.API.Context
+﻿using CarRepairWorkshop.Shared;
+using Microsoft.EntityFrameworkCore;
+
+public class WorkshopDbContext : DbContext
 {
-    public class DbContext
+    public WorkshopDbContext(DbContextOptions<WorkshopDbContext> options): base(options){ }
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<WorkOrder> WorkOrders { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.Entity<Customer>()
+            .ToTable("Customer")
+            .HasKey(c => c.Id);
+
+        modelBuilder.Entity<WorkOrder>()
+            .ToTable("WorkOrder")
+            .HasKey(c => c.Id);
     }
 }
