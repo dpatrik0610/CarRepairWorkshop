90bb8eafb98faf3655039dda32582111d64dc909
dpatrik0610 <d.patrik0610@gmail.com>
2023-11-26 19:11:40 +0100

    Created IWorkEstimationService

---
+++ CarRepairWorkshop/Services/Interfaces/IWorkEstimationService.cs
@@ -0,0 +1,10 @@
+﻿namespace CarRepairWorkshop.API.Services.Interfaces
+{
+    public interface IWorkEstimationService
+    {
+        int getWightMultiplierByCategory();
+        double getWeightMultiplierByAge();
+        double getWeightMultiplierByDamage();
+        double CalculateWorkHourEstimation(string category, int ageOfCar, int damageSeverity);
+    }
+}
