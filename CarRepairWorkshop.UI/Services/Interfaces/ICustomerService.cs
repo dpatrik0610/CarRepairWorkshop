24fefb794b5eb9c6329ac6f214f3f5b0908811f5
dpatrik0610 <d.patrik0610@gmail.com>
2023-11-27 20:21:32 +0100

    Preparing frontend, added ICustomerService and IWorkOrderService to Services

---
+++ CarRepairWorkshop.UI/Services/Interfaces/ICustomerService.cs
@@ -0,0 +1,14 @@
+﻿using CarRepairWorkshop.Shared;
+
+namespace CarRepairWorkshop.UI.Services.Interfaces
+{
+    public interface ICustomerService
+    {
+        Task<IEnumerable<Customer>?> GetCustomersAsync();
+        Task<Customer?> GetCustomerByIdAsync(int customerId);
+        Task<Customer?> GetCustomerByNameAsync(string customerName);
+        Task AddCustomerAsync(Customer customer);
+        Task UpdateCustomerAsync(Customer customer);
+        Task DeleteCustomerAsync(int customerId);
+    }
+}
---
+++ CarRepairWorkshop.UI/Services/Interfaces/IWorkOrderService.cs
@@ -0,0 +1,14 @@
+﻿using CarRepairWorkshop.Shared;
+
+namespace CarRepairWorkshop.UI.Services.Interfaces
+{
+    public interface IWorkOrderService
+    {
+        Task<IEnumerable<WorkOrder>?> GetWorkOrdersAsync();
+        Task<IEnumerable<WorkOrder>?> GetWorkOrdersByCustomerIdAsync(string workOrderName);
+        Task<WorkOrder?> GetWorkOrderByIdAsync(int workOrderId);
+        Task AddWorkOrderAsync(WorkOrder workOrder);
+        Task UpdateWorkOrderAsync(WorkOrder workOrder);
+        Task DeleteWorkOrderAsync(int workOrderId);
+    }
+}
