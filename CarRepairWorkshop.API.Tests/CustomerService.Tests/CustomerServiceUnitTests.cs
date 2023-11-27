b525150ddcd9833a4d8abe6315d6c42bd3172a39
dpatrik0610 <d.patrik0610@gmail.com>
2023-11-27 09:59:11 +0100

    Added more Unit Tests for Customer Service

--- CarRepairWorkshop.API.Tests/CustomerService.Tests/CustomerServiceUnitTests.cs
+++ CarRepairWorkshop.API.Tests/CustomerService.Tests/CustomerServiceUnitTests.cs
@@ -27,7 +27,6 @@ public class CustomerServiceUnitTests
         // Add customers to the database
         var customer = new Customer
         {
-            Id = Guid.NewGuid(),
             Name = $"{name}",
             Address = $"{address}",
             Email = $"{email}"
@@ -67,5 +66,120 @@ public class CustomerServiceUnitTests
         // Assert
         Assert.NotNull(result);
         Assert.Equal(customerId, result.Id);
+    }
+
+    [Fact]
+    public async Task GetCustomerByNameAsync_ReturnsCorrectCustomer()
+    {
+        // Arrange
+        WorkshopDbContext dbContext = GenerateTestContext();
+        var logger = Substitute.For<ILogger<CustomerService>>();
+        var service = new CustomerService(logger, dbContext);
+
+        var customerName = "John Doe";
+        var customer = new Customer
+        {
+            Name = customerName,
+            Address = "123 Main St",
+            Email = "john.doe@example.com"
+        };
+        dbContext.Customers.Add(customer);
+        await dbContext.SaveChangesAsync();
+
+        // Act
+        var result = await service.GetCustomerByNameAsync(customerName);
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(customerName, result.Name);
+    }
+
+    [Fact]
+    public async Task AddCustomerAsync_AddsCustomerToDatabase()
+    {
+        // Arrange
+        WorkshopDbContext dbContext = GenerateTestContext();
+        var logger = Substitute.For<ILogger<CustomerService>>();
+        var service = new CustomerService(logger, dbContext);
+
+        var customer = new Customer
+        {
+            Name = "John Doe",
+            Address = "123 Main St",
+            Email = "john.doe@example.com"
+        };
+
+        // Act
+        await service.AddCustomerAsync(customer);
+
+        // Assert
+        var result = await dbContext.Customers.FirstOrDefaultAsync(c => c.Id == customer.Id);
+        Assert.NotNull(result);
+        Assert.Equal(customer.Name, result.Name);
+    }
+
+    [Fact]
+    public async Task DeleteCustomerAsync_DeletesCustomerFromDatabase()
+    {
+        // Arrange
+        WorkshopDbContext dbContext = GenerateTestContext();
+        var logger = Substitute.For<ILogger<CustomerService>>();
+        var service = new CustomerService(logger, dbContext);
+
+        var customerId = Guid.NewGuid();
+        var customer = new Customer
+        {
+            Id = customerId,
+            Name = "John Doe",
+            Address = "123 Main St",
+            Email = "john.doe@example.com"
+        };
+        dbContext.Customers.Add(customer);
+        await dbContext.SaveChangesAsync();
+
+        // Act
+        await service.DeleteCustomerAsync(customerId);
+
+        // Assert
+        var result = await dbContext.Customers.FirstOrDefaultAsync(c => c.Id == customerId);
+        Assert.Null(result);
+    }
+
+    [Fact]
+    public async Task UpdateCustomerDataAsync_UpdatesCustomerInDatabase()
+    {
+        // Arrange
+        WorkshopDbContext dbContext = GenerateTestContext();
+        var logger = Substitute.For<ILogger<CustomerService>>();
+        var service = new CustomerService(logger, dbContext);
+
+        var customerId = Guid.NewGuid();
+        var customer = new Customer
+        {
+            Id = customerId,
+            Name = "John Doe",
+            Address = "123 Main St",
+            Email = "john.doe@example.com"
+        };
+        dbContext.Customers.Add(customer);
+        await dbContext.SaveChangesAsync();
+
+        var updatedCustomerData = new Customer
+        {
+            Id = customerId,
+            Name = "Updated Name",
+            Address = "456 New St",
+            Email = "updated.email@example.com"
+        };
+
+        // Act
+        await service.UpdateCustomerDataAsync(updatedCustomerData);
+
+        // Assert
+        var result = await dbContext.Customers.FirstOrDefaultAsync(c => c.Id == customerId);
+        Assert.NotNull(result);
+        Assert.Equal(updatedCustomerData.Name, result.Name);
+        Assert.Equal(updatedCustomerData.Address, result.Address);
+        Assert.Equal(updatedCustomerData.Email, result.Email);
     }
 }
