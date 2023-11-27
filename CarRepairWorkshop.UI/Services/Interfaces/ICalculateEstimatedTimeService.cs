e39a49ade4883b7551e2e2252341bd8c183e95d6
dpatrik0610 <d.patrik0610@gmail.com>
2023-11-27 23:11:41 +0100

    Removed unnessecary files, Added UI for listing customers, accessing and modifying their details.

--- CarRepairWorkshop.UI/Services/Interfaces/ICalculateEstimatedTimeService.cs
+++
@@ -1,9 +0,0 @@
-﻿using CarRepairWorkshop.Shared;
-
-namespace CarRepairWorkshop.UI.Services.Interfaces
-{
-    public interface ICalculateEstimatedTimeService
-    {
-        Task<double?> CalculateEstimatedTime(WorkOrder workOrder);
-    }
-}
